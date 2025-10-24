3f8f64ab9af1112961055522f9e31d4916b659d4
rubyi <rubyi@LAPTOP-8UAD3FDM>
2025-10-24 22:25:20 +0700

    Add project files.

---
+++ PrivateWorkshop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PrivateWorkshop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
